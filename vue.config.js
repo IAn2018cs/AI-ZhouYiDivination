4d6750831cb24c98fd50aa72a46f156c930fb032
chenshuai <chenshuai@ian2018.cn>
2024-09-13 00:41:02 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
     devServer: {
       proxy: {
         '/divinate': {
-          target: 'http://localhost:3000',
+          target: 'http://localhost:3002',
           changeOrigin: true
         },
         '/divinate-test': {
-          target: 'http://localhost:3000',
+          target: 'http://localhost:3002',
           changeOrigin: true
         }
       }
